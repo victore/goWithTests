6d86d5ac16410772187cda9ef716f53ad98edb8d
Victor Guadarrama <victor.guadarrama@infosel.com>
2022-03-23 23:47:48 -0600

    more refactor

--- 01_hello/hello.go
+++ 01_hello/hello.go
@@ -8,25 +8,27 @@ const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 
-// This function returns a string
+// main function
+func main() {
+	fmt.Println(Hello("Elodia", "French"))
+}
+
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
-}
-
-// main function
-func main() {
-	fmt.Println(Hello("Elodia", "French"))
+	return
 }
